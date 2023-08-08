672be86e04bd6365eadc453be45af864dfac14cd
Alex <argeares97@gmail.com>
2023-08-08 14:54:59 +0300

    fix: app chain ids

--- src/utils/appConfig.ts
+++ src/utils/appConfig.ts
@@ -11,7 +11,12 @@ export const appConfig: {
   providers: Record<number, StaticJsonRpcBatchProvider>;
   contractAddresses: Record<number, string>;
 } = {
-  chainIds: [mainnetChainId, polygonChainId],
+  chainIds: [
+    mainnetChainId,
+    polygonChainId,
+    avalancheChainId,
+    OPMainnetChainId,
+  ],
   providers: {
     [mainnetChainId]:
       chainInfoHelper.providerInstances[mainnetChainId].instance,
